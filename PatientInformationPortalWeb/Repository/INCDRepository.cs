9eb3f16ad79f4f6888249c71a8b19663e9305155
Sujit2444 <sujitkumarghosh70@gmail.com>
2024-02-26 21:38:09 +0600

    Add NCD Repo

---
+++ PatientInformationPortalWeb/Repository/INCDRepository.cs
@@ -0,0 +1,9 @@
+﻿using PatientInformationPortalWeb.Models;
+
+namespace PatientInformationPortalWeb.Repository
+{
+    public interface INCDRepository
+    {
+        Task<List<NCD>> GetAllNCDs();
+    }
+}
---
+++ PatientInformationPortalWeb/Repository/NCDRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatientInformationPortalWeb.Data;
+using PatientInformationPortalWeb.Models;
+
+namespace PatientInformationPortalWeb.Repository
+{
+    public class NCDRepository:INCDRepository
+    {
+        private readonly ApplicationDBContext _applicationDBContext;
+        public NCDRepository(ApplicationDBContext applicationDBContext)
+        {
+            _applicationDBContext = applicationDBContext;
+        }
+        public async Task<List<NCD>> GetAllNCDs()
+        {
+            return await _applicationDBContext.NCDs.ToListAsync();
+            //throw new NotImplementedException();
+        }
+    }
+}

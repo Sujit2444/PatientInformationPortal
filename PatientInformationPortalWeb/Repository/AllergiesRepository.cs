636d4dadcf270ac47c9276f953bee43512eaba05
Sujit2444 <sujitkumarghosh70@gmail.com>
2024-02-26 21:36:44 +0600

    Add Allergies Repository

---
+++ PatientInformationPortalWeb/Repository/AllergiesRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatientInformationPortalWeb.Data;
+using PatientInformationPortalWeb.Models;
+
+namespace PatientInformationPortalWeb.Repository
+{
+    public class AllergiesRepository:IAllergiesRepository
+    {
+        private readonly ApplicationDBContext _applicationDBContext;
+        public AllergiesRepository(ApplicationDBContext applicationDBContext)
+        {
+            _applicationDBContext = applicationDBContext;
+        }
+        public async Task<List<Allergies>> GetAllAllergies()
+        {
+            return await _applicationDBContext.Allergies.ToListAsync();
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ PatientInformationPortalWeb/Repository/IAllergiesRepository.cs
@@ -0,0 +1,9 @@
+﻿using PatientInformationPortalWeb.Models;
+
+namespace PatientInformationPortalWeb.Repository
+{
+    public interface IAllergiesRepository
+    {
+        Task<List<Allergies>> GetAllAllergies();
+    }
+}

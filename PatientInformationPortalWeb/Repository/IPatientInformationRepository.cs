0d7a811a38bc08fa8eee60739f86c32ccd1d0164
Sujit2444 <sujitkumarghosh70@gmail.com>
2024-02-26 15:25:30 +0600

    Adding repository pattern

---
+++ PatientInformationPortalWeb/Repository/IPatientInformationRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using PatientInformationPortalWeb.Models;
+
+namespace PatientInformationPortalWeb.Repository
+{
+    public interface IPatientInformationRepository
+    {
+        Task<IEnumerable<PatientInformation>> GetAllPatientInformation();
+        Task<PatientInformation> GetPatientInformationById(int id);
+        Task AddPatient(PatientInformation patientInformation);
+        Task UpdatePatient(PatientInformation patientInformation);
+
+        Task DeletePatient(int id);
+    }
+}
---
+++ PatientInformationPortalWeb/Repository/PatientInformationRepository.cs
@@ -0,0 +1,39 @@
+﻿using PatientInformationPortalWeb.Data;
+using PatientInformationPortalWeb.Models;
+
+namespace PatientInformationPortalWeb.Repository
+{
+    public class PatientInformationRepository : IPatientInformationRepository
+    {
+        private readonly ApplicationDBContext _applicationDBContext;
+        public PatientInformationRepository(ApplicationDBContext applicationDBContext)
+        {
+            _applicationDBContext = applicationDBContext;
+        }
+
+        public Task AddPatient(PatientInformation patientInformation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePatient(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PatientInformation>> GetAllPatientInformation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PatientInformation> GetPatientInformationById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdatePatient(PatientInformation patientInformation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

0acbc0306fc30d3ae1cf10f34ff50538764a7940
Sujit2444 <sujitkumarghosh70@gmail.com>
2024-02-27 18:32:04 +0600

    refactor js code changes

--- PatientInformationPortalWeb/wwwroot/js/site.js
+++ PatientInformationPortalWeb/wwwroot/js/site.js
@@ -1,26 +1,31 @@
-﻿ $(document).ready(function () {
-        $("#addNCDButton").on('click', function (e) {
-            e.preventDefault();
-            let selectedItems = $("#leftNCDs option:selected");
-            selectedItems.detach().appendTo("#rightNCDs");
-        });
+﻿$(document).ready(function () {
+    $("#rightNCDs option").prop("selected", true);
+    $('#rightAllergies option').prop("selected", true);
+
+    $("#addNCDButton").on('click', function (e) {
+        e.preventDefault();
+        let selectedItems = $("#leftNCDs option:selected");
+        selectedItems.detach().appendTo("#rightNCDs");
+    });
 
     $("#removeNCDButton").on('click', function (e) {
         e.preventDefault();
-    let selectedItems = $("#rightNCDs option:selected");
-    selectedItems.detach().appendTo("#leftNCDs");
-        });
+        let selectedItems = $("#rightNCDs option:selected");
+        selectedItems.detach().appendTo("#leftNCDs");
+        $("#rightNCDs option").prop("selected", true);
+    });
 
     $("#addAlButton").on('click', function (e) {
         e.preventDefault();
-    let selectedItems = $("#leftAllergies option:selected");
-    selectedItems.detach().appendTo("#rightAllergies");
-        });
+        let selectedItems = $("#leftAllergies option:selected");
+        selectedItems.detach().appendTo("#rightAllergies");
+    });
 
     $("#removeAlButton").on('click', function (e) {
         e.preventDefault();
-    let selectedItems = $("#rightAllergies option:selected");
-    selectedItems.detach().appendTo("#leftAllergies");
-        });
-
+        let selectedItems = $("#rightAllergies option:selected");
+        selectedItems.detach().appendTo("#leftAllergies");
+        $('#rightAllergies option').prop("selected", true);
     });
+
+});

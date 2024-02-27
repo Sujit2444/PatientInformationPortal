ddccfaa4b93e22db8d86f58d84e99dbb7074dbf5
Sujit2444 <sujitkumarghosh70@gmail.com>
2024-02-27 12:44:50 +0600

    ef core migration files

---
+++ PatientInformationPortalWeb/Migrations/20240226200432_removekeyannotation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatientInformationPortalWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class removekeyannotation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

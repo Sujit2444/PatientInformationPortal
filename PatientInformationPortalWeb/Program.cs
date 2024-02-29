68a6fd9516bfdd5a0a6501f18607cc53bc28a9b3
Sujit2444 <sujitkumarghosh70@gmail.com>
2024-02-29 12:27:15 +0600

    program file changes

--- PatientInformationPortalWeb/Program.cs
+++ PatientInformationPortalWeb/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IDiseaseInformationRepository,DiseaseInformationRepository>();
+builder.Services.AddScoped<IDiseaseInformationRepository, DiseaseInformationRepository>();
 builder.Services.AddScoped<INCDRepository, NCDRepository>();
 builder.Services.AddScoped<IAllergiesRepository, AllergiesRepository>();
 builder.Services.AddScoped<IPatientInformationRepository, PatientInformationRepository>();
